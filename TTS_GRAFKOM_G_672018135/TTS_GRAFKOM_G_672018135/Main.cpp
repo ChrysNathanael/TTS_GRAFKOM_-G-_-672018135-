db52f241055846628d21ae7f8cb5cd41b6de3a99
ChrysNathanael <45379004+ChrysNathanael@users.noreply.github.com>
2020-06-18 18:32:19 +0700

    Add files via upload

--- TTS_GRAFKOM_G_672018135/TTS_GRAFKOM_G_672018135/Main.cpp
+++ TTS_GRAFKOM_G_672018135/TTS_GRAFKOM_G_672018135/Main.cpp
@@ -1,41 +1,67 @@
 #include <iostream>
 #include <GL/freeglut.h>
 
+int x = 0;
+int y = 0;
+
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glPointSize(150);
-	glBegin(GL_POINTS);
-	glColor3f(0, 0, 0);
-	glVertex2f(1,1);
-	glEnd();
+	for (int i = 0; i <= 5; i++) {
+		x = 0;
+		for (int r = 0; r <= 5; r++) {
 
-	glPointSize(90);
-	glBegin(GL_POINTS);
-	glColor3f(1, 0, 0);
-	glVertex2f(50, 50);
-	glEnd();
-	glFlush();
+			glColor3f(0, 0, 0);
+			glBegin(GL_QUADS);
+			glVertex2f(5 + x, 75 + y); //kiri bawah
+			glVertex2f(5 + x, 5 + y);//titik kiri atas
+			glVertex2f(75 + x, 5 + y);//kanan bawah
+			glVertex2f(75 + x, 75 + y);//kanan atas
+			glEnd();
+
+			glPointSize(70);
+			glBegin(GL_POINTS);
+			glColor3f(1, 1, 1);
+			glVertex2f(110 + x, 40 + y);
+			glEnd();
+
+			glPointSize(70);
+			glBegin(GL_POINTS);
+			glColor3f(1, 1, 1);
+			glVertex2f(40 + x, 110 + y);
+			glEnd();
+
+			glColor3f(0, 0, 0);
+			glBegin(GL_POLYGON);
+			glVertex2f(75 + x, 145 + y); //kiri bawah
+			glVertex2f(75 + x, 75 + y);//titik kiri atas
+			glVertex2f(145 + x, 75 + y);//kanan bawah
+			glVertex2f(145 + x, 145 + y);//kanan atas
+			glEnd();
+
+			x += 145;
+		}
+		y += 143;
+	}
+	glutSwapBuffers();
 }
 
 void myinit() {
-	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClearColor(1.0, 0.5, 0.0, 0.0);
 	glColor3f(1.0, 0.0, 0.0);
 	glPointSize(2.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0, 100, 100, 0);// left,right,bottom,top
+	gluOrtho2D(0, 500, 500, 00);// left,right,bottom,top
 }
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Points");
+	glutCreateWindow("Made By 672018135");
 	glutDisplayFunc(display);
-
 	myinit();
 	glutMainLoop();
-
 	return 0;
 }
